b29eb1a63f43c6e459b83149671af62a9d7ce826
ZoroZero <40843946+ZoroZero@users.noreply.github.com>
2020-06-04 09:05:56 +0700

    Add loginActivity class and layout

---
+++ Smart_garden_app/app/src/main/java/Login_RegisterUser/LoginActivity.java
@@ -0,0 +1,16 @@
+package Login_RegisterUser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartgarden.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
