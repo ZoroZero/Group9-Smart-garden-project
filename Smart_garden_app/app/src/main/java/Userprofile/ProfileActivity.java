2bb4d7c04d28b45a2987891070c4222d795b93ea
ZoroZero <40843946+ZoroZero@users.noreply.github.com>
2020-06-08 10:52:49 +0700

    Add menu to navigate app
    + add menu directory in res
    + Add icon in drawable

--- Smart_garden_app/app/src/main/java/Userprofile/ProfileActivity.java
+++ Smart_garden_app/app/src/main/java/Userprofile/ProfileActivity.java
@@ -1,9 +1,16 @@
 package Userprofile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -40,5 +47,49 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
         //DatabaseHelper.FetchDevicesInfo(this, this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.normal_menu, menu);
+        menu.add(0, 1, 1,
+                menuIconWithText(getResources().getDrawable(R.drawable.ic_exit_to_app_black_24dp),
+                        getResources().getString(R.string.log_out)));
+
+        menu.add(0, 2, 2,
+                menuIconWithText(getResources().getDrawable(R.drawable.ic_add_black_24dp),
+                        getResources().getString(R.string.register_device)));
+
+        menu.add(0, 3, 3,
+                menuIconWithText(getResources().getDrawable(R.drawable.ic_local_florist_black_24dp),
+                        getResources().getString(R.string.register_plant)));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                UserLoginManagement.getInstance(this).logOut();
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+                return true;
+            case 2:
+                startActivity(new Intent(getApplicationContext(), RegisterDeviceSearchActivity.class));
+                finish();
+                return true;
+            case 3:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private CharSequence menuIconWithText(Drawable r, String title) {
+        r.setBounds(0, 0, r.getIntrinsicWidth(), r.getIntrinsicHeight());
+        SpannableString sb = new SpannableString("    " + title);
+        ImageSpan imageSpan = new ImageSpan(r, ImageSpan.ALIGN_BOTTOM);
+        sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return sb;
     }
 }
